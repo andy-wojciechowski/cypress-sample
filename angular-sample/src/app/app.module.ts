ec73fed3ea03aa20d8ba1d11fe174a24a3945562
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-09-01 14:44:38 -0500

    Started implementing the sample component

--- angular-sample/src/app/app.module.ts
+++ angular-sample/src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { SampleComponent } from './sample/sample.component';
@@ -10,7 +17,13 @@ import { SampleComponent } from './sample/sample.component';
     SampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-sample/src/app/sample/sample.component.ts
+++ angular-sample/src/app/sample/sample.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sample',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sample.component.css']
 })
 export class SampleComponent implements OnInit {
+  public selectOptions: Array<string>;
+  public selectControl: FormControl = new FormControl('', [Validators.required]);
+  public inputControl: FormControl = new FormControl('', [Validators.required])
+  public valueSelected: boolean;
+  public checkbox1Selected: boolean;
+  public checkbox2Selected: boolean;
+  public checkbox3Selected: boolean;
+  public submissionMessage: string;
+  public sampleForm: FormGroup = new FormGroup({
+    selectControl: this.selectControl,
+    inputControl: this.inputControl
+  });
 
   constructor() { }
 
   ngOnInit() {
+    this.selectOptions = ['Option 1', 'Option 2', 'Option 3'];
   }
 
+  public onSelectionChange(): void {
+    if(this.selectControl.value !== null) {
+      this.valueSelected = true;
+    }
+  }
+
+  public onSubmit(): void {
+    this.submissionMessage = 'Form Submited!';
+  }
 }
