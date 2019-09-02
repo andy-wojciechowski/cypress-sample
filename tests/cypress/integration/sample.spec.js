a3d3bf11d2999817ef3e356196d108efdb36ce6d
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-09-02 15:28:23 -0500

    Implemented a sample spec

--- tests/cypress/integration/sample.spec.js
+++ tests/cypress/integration/sample.spec.js
@@ -1,3 +1,19 @@
 describe('Sample component works as expected', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:4200');
+    });
 
+    it('should be able to submit the form correctly', () => {
+        // Arrange
+        cy.get('#matSelectSample').click();
+        cy.get('mat-option').contains('Option 1').click();
+        cy.get('#requiredInput').type('sample');
+
+        // Act
+        cy.get('#submitButton').click();
+
+        // Assert
+        cy.get('#submissionMessage').should('exist');
+        cy.get('#submissionMessage').should('have.text', 'Form Submited!');
+    });
 });
